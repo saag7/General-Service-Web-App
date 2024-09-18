19b44867e3066288ef5858343220bc9de2dd82d8
saag7 <salomon.agboto7@gmail.com>
2024-09-18 11:20:35 +0100

    uploading files

---
+++ PHP/Accueil.php
@@ -0,0 +1,57 @@
+ <?php 
+ include('../PHP/header.php');
+
+ ?>
+ 
+ <body>
+
+<!----------------------------------------------page d'accueeil start in affiliatee markeeting-->
+<section id="news">
+        <div class="start">
+            <h2> <span class="affliate"> AFFILIATE MARKETING</span><br> For all Service in informatique of development</h2>
+            <p class="paragraghe">No matter what type of service <br> you want in computer science and digital electronics,<br> we offer you the best quality company and speed of service <br> to approach</p>
+        </div>
+        <div>
+            <a href="#merchant" class="btn-primary btn-lg mr-2">For Entreprise</a>
+            <a href="#affiliates" class="btn-light btn-lg ">For Affiliate</a>
+        </div>
+
+    </section>
+    <section id="feature" class="section-p1">
+        <div class="fe-box">
+            <img src="../img/marketing.svg" width="120" height="80" alt="">
+            <h6> free Marketing</h6>
+        </div>
+        <div class="fe-box">
+            <img src="../img/features-affiliate-tracking.svg" width="120" height="80" alt="">
+            <h6>Online Marketing</h6>
+        </div>
+        <div class="fe-box">
+            <img src="../img/features-customization.svg" width="120" height="80" alt="">
+            <h6>Money Marketing</h6>
+        </div>
+        <div class="fe-box">
+            <img src="../img/features-analytics.svg" width="120" height="80" alt="">
+            <h6> promotions Marketing</h6>
+        </div>
+        <div class="fe-box">
+            <img src="../img/nd.png" width="120" height="80" alt="">
+            <h6>Happy Marketing</h6>
+        </div>
+        <div class="fe-box">
+            <img src="../img/news.png" width="120" height="80" alt="">
+            <h6>Support Marketing</h6>
+        </div>
+    </section>
+    <!------------------------------------------------------en affiliate marketing-->
+
+
+</body>
+
+<?php
+
+include('../PHP/footer.php');
+?>
+
+
+ 
---
+++ PHP/header.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="../Css/style.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!--CSS END-->
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!--javaScript END-->
+    <title>
+        Affiliate marketing
+    </title>
+    
+
+
+</head>
+
+<body>
+    <!-------------started the header -->
+    <header id="header">
+        <section>
+            <nav class="navbar navbar-expand-lg navbar-light bg-info">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php"> <img src="../img/logo.png" width="80px" height="80px" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul id="nadbar" class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="catalog.php">Catalog</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">Service</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li>
+                                        <a class="dropdown-item" href="info.php">Informatique</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="elec.php">Electronique</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li>
+                                        <a id="icon" class="dropdown-item" href="ecom.php"> <i class="fa-solid fa-bag-shopping"></i> E-comerce</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact us</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.php">Sign in/Sign up</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </form>
+                    </div>
+
+                </div>
+            </nav>
+        </section>
+    </header>
+    <!-----------------------header end navbar-->
+   
+
+<!----- footer debut--->
+
+
+
+
+<!------ footer fin--->
+
+
+
+</body>
+
+</html>
